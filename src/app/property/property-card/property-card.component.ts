bf6287a2d813485ab71222cb22782238d93e90a7
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-03-24 21:14:34 -0500

    Update property-card.component.ts
    Css styles have been added to the template within your created folder

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
 
     selector: 'app-property-card',
-    template: `<h1>This is kenneth david Aboagye </h1>`
+    template: `<h1>This is kenneth david Aboagye </h1>`,
+    styles: ['h1 {font-weight: normal;}']
 
 }
 )
