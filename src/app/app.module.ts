e485f1fd4de21fc59eead1a6f7801ee85352cafc
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-03-24 20:53:55 -0500

    add property card component and disable analytics in angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   // Ensure this line ends with a comma
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PropertyCardComponent 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'app-property-card',
+    template: `<h1>This is kenneth david </h1>`
+
+}
+)
+export class PropertyCardComponent {
+
+}
