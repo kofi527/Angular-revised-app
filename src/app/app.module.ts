702a5c4aa374d2460c0d7a34307cf477b96db23c
kofi527 <63665356+kofi527@users.noreply.github.com>
2025-03-26 16:41:42 -0500

    update in app.module file
    creating a property list component which is automatically added to the declaration file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
  
 
 @NgModule({
   declarations: [
     AppComponent,
+    PropertyListComponent,
    // Ensure this line ends with a comma
   ],
   imports: [
